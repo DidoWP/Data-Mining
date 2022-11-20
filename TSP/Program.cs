1c5188728bbdd5cb845b68684de92b0d2fe39cd6
Daniel Dimitrov <56864621+DidoWP@users.noreply.github.com>
2022-11-20 22:10:09 +0200

    create project

---
+++ TSP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+
+class Program
+{
+    public static void Main()
+    {
+        var n = 10;
+        var towns = new string[10]{"A","B","C","D","E","F","G","H","J","K"};
+        var positions = new List<KeyValuePair<int,int>>();
+
+        var rand = new Random();
+
+        while(n > 0)
+        {
+            
+        }
+    }
+}
+
